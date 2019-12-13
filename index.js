8876de273a56172f9dd66f511bfc89a6d6fb6232
Yanchenko Serhii <run2cb@gmail.com>
2019-12-13 12:55:55 +0200

    edit index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express')
 const app = express()
 
@@ -7,21 +6,3 @@ app.get('/', (req, res) => res.send('Hello World!'))
 var server = app.listen(9001, () => {
     console.log("Listening on port " + server.address().port + "...");
 });
-=======
-'use strict';
-
-const express = require('express');
-
-// Constants
-const PORT = 9000;
-const HOST = '127.0.0.1';
-
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello devops53 group');
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
->>>>>>> dev
