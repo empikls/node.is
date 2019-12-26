17b06d8ce0343336b1aca0b76a8b8cbcefb81815
Yanchenko Serhii <run2cb@gmail.com>
2019-12-26 16:32:39 +0200

    edit node.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send(' Proverka v1.3'))
+app.get('/', (req, res) => res.send(' Proverka v1.4'))
 
 var server = app.listen(9001, () => {
     console.log("Listening on port " + server.address().port + "...");
