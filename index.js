3628b81f6b9e6b04ba1bc76da31f44a8f0728c6a
Yanchenko Serhii <run2cb@gmail.com>
2019-12-26 13:57:11 +0200

    edit node.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send(' World!'))
+app.get('/', (req, res) => res.send(' NAKONECZTO INGRESS VIPUSTILSYA!'))
 
 var server = app.listen(9001, () => {
     console.log("Listening on port " + server.address().port + "...");
