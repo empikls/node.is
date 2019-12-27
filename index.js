7a4a11c7e71df611b2ab3d25fb2c4f3ed3bec9a7
empikls <52194104+empikls@users.noreply.github.com>
2019-12-27 15:23:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send(' App version2'))
+app.get('/', (req, res) => res.send(' App version PR'))
 
 var server = app.listen(9001, () => {
     console.log("Listening on port " + server.address().port + "...");
